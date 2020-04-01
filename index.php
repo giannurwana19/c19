4b172e03db914e1fa1f5d179482d0ffa4d5238da
giannurwana19 <giannurwana19@gmail.com>
2020-04-02 01:33:45 +0700

    capek, fix di indonesia

--- index.php
+++ index.php
@@ -9,12 +9,13 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
-  <title>Hello, world!</title>
+
+  <title>Lost Corona - Live Data </title>
 </head>
 
 <body>
   <!-- navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-info fixed-top">
     <div class="container">
 
       <a class="navbar-brand font-weight-bold" href="#">
@@ -25,10 +26,10 @@
       </button>
       <div class="collapse navbar-collapse font-weight-bold" id="navbarNavAltMarkup">
         <div class="navbar-nav ml-auto">
-          <a class="nav-item nav-link active mx-2" href="#beranda">Beranda <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link mx-2" href="#dunia">Dunia</a>
-          <a class="nav-item nav-link mx-2" href="#">Indonesia</a>
+          <a class="nav-item nav-link active mx-2" href="#home">Beranda <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link mx-2" href="#indonesia">Indonesia</a>
           <a class="nav-item nav-link mx-2" href="#">Provinsi</a>
+          <a class="nav-item nav-link mx-2" href="#dunia">Dunia</a>
           <a class="nav-item nav-link mx-2" href="#">Tentang Saya</a>
         </div>
       </div>
@@ -38,7 +39,7 @@
   <!-- end navbar -->
 
   <!-- jumbotron -->
-  <section class="pt-5" id="beranda">
+  <section class="py-5 mb-5" id="home">
     <div class="container pt-5">
       <div class="row align-items-center">
 
@@ -46,11 +47,11 @@
           <h1 class="font-weight-bold">Lost-Corona! </h1>
           <h4 class="font-weight-bold">#dirumahAja</h4>
           <p class="text-justify">Live data persebaran Virus Corona atau COVID-19 di Dunia. Data di website ini akan selalu ter <b><i>update</i></b> secara <b><i>real-time</i></b> melalui sumber website resmi dari <a href="https://kawalcorona.com">kawalcorona.com</a></p>
-          <a href="#" class="btn btn-info font-weight-bold px-5 mt-3 mr-3 rounded-0">Data Indonesia</a>
-          <a href="#" class="btn btn-outline-info font-weight-bold px-5 mt-3 rounded-0">Data Dunia</a>
+          <a href="#indonesia" class="btn btn-info font-weight-bold px-4 mt-3 mr-3 rounded-0">Data Indonesia</a>
+          <a href="#" class="btn btn-outline-info font-weight-bold px-4 mt-3 rounded-0">Data Dunia</a>
         </div>
         <div class="col-md-5 col-lg-6 order-1 order-md-2">
-          <img src="img/dokter.png" alt="" class="img-fluid">
+          <img src="img/doctor.png" alt="" class="img-fluid">
         </div>
 
       </div>
@@ -60,37 +61,37 @@
 
 
   <!-- section dunia -->
-  <section id="dunia" class="py-5">
+  <section id="indonesia" class="bg-light py-5">
     <div class="container py-5">
       <div class="row mb-5">
         <div class="col text-center">
           <h1 class="font-weight-bold">INDONESIA</h1>
-          <p class="lead mb-4">Info Corona atau COVID-19 di Negara Indonesia secara keseluran dari sabang sampai merauke</p>
+          <p class="lead mb-4">Info Corona atau COVID-19 di Negara Indonesia secara keseluran</p>
           <div class="row">
-            <div class="col-lg">
-              <div class="card">
+            <div class="col-lg mb-3">
+              <div class="card text-white" style="background-color: #fd9644">
                 <div class="card-body">
-                  <img src="img/senang.png" width="100" class="mb-4" alt="">
-                  <h4 class="card-title">Sembuh</h5>
-                  <h5 class="card-subtitle mb-2 text-muted">185 Orang</h6>
+                  <img src="img/positif.png" width="100" class="mb-4" alt="">
+                  <h4 class="card-title">Positif</h5>
+                    <h5 class="card-subtitle mb-2 ">185 Orang</h6>
                 </div>
               </div>
             </div>
-            <div class="col-lg">
-              <div class="card">
+            <div class="col-lg mb-3">
+              <div class="card text-white" style="background-color: #26de81">
                 <div class="card-body">
                   <img src="img/senang.png" width="100" class="mb-4" alt="">
                   <h4 class="card-title">Sembuh</h5>
-                  <h5 class="card-subtitle mb-2 text-muted">185 Orang</h6>
+                    <h5 class="card-subtitle mb-2 ">185 Orang</h6>
                 </div>
               </div>
             </div>
-            <div class="col-lg">
-              <div class="card">
+            <div class="col-lg mb-3">
+              <div class="card text-white" style="background-color: #fc5c65">
                 <div class="card-body">
-                  <img src="img/senang.png" width="100" class="mb-4" alt="">
-                  <h4 class="card-title">Sembuh</h4>
-                  <h5 class="card-subtitle mb-2 text-muted">185 Orang</h5>
+                  <img src="img/meninggal.png" width="100" class="mb-4" alt="">
+                  <h4 class="card-title">Meninggal</h4>
+                  <h5 class="card-subtitle mb-2 ">185 Orang</h5>
                 </div>
               </div>
             </div>
@@ -99,6 +100,27 @@
       </div>
   </section>
   <!-- end section dunia -->
+
+  <section>
+    <table id="table_id" class="display">
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Row 1 Data 1</td>
+          <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+          <td>Row 2 Data 1</td>
+          <td>Row 2 Data 2</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 
 
 
@@ -112,6 +134,7 @@
   <script src="js/jquery.min.js"></script>
   <script src="js/popper.min.js"></script>
   <script src="bootstrap/js/bootstrap.min.js"></script>
+  </script>
 </body>
 
 </html>
