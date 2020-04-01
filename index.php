7862ceca9d4940d1787012a93d56f751451d979e
giannurwana19 <giannurwana19@gmail.com>
2020-04-02 00:11:51 +0700

    inisialiasi dan bagian section indonesia

---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container">
+
+      <a class="navbar-brand font-weight-bold" href="#">
+        <h1 class="font-weight-bold m-0">Nurwana</h1>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse font-weight-bold" id="navbarNavAltMarkup">
+        <div class="navbar-nav ml-auto">
+          <a class="nav-item nav-link active mx-2" href="#beranda">Beranda <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link mx-2" href="#dunia">Dunia</a>
+          <a class="nav-item nav-link mx-2" href="#">Indonesia</a>
+          <a class="nav-item nav-link mx-2" href="#">Provinsi</a>
+          <a class="nav-item nav-link mx-2" href="#">Tentang Saya</a>
+        </div>
+      </div>
+
+    </div>
+  </nav>
+  <!-- end navbar -->
+
+  <!-- jumbotron -->
+  <section class="pt-5" id="beranda">
+    <div class="container pt-5">
+      <div class="row align-items-center">
+
+        <div class="col-md-7 col-lg-6 order-2 order-md-1">
+          <h1 class="font-weight-bold">Lost-Corona! </h1>
+          <h4 class="font-weight-bold">#dirumahAja</h4>
+          <p class="text-justify">Live data persebaran Virus Corona atau COVID-19 di Dunia. Data di website ini akan selalu ter <b><i>update</i></b> secara <b><i>real-time</i></b> melalui sumber website resmi dari <a href="https://kawalcorona.com">kawalcorona.com</a></p>
+          <a href="#" class="btn btn-info font-weight-bold px-5 mt-3 mr-3 rounded-0">Data Indonesia</a>
+          <a href="#" class="btn btn-outline-info font-weight-bold px-5 mt-3 rounded-0">Data Dunia</a>
+        </div>
+        <div class="col-md-5 col-lg-6 order-1 order-md-2">
+          <img src="img/dokter.png" alt="" class="img-fluid">
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- end jumbotron -->
+
+
+  <!-- section dunia -->
+  <section id="dunia" class="py-5">
+    <div class="container py-5">
+      <div class="row mb-5">
+        <div class="col text-center">
+          <h1 class="font-weight-bold">INDONESIA</h1>
+          <p class="lead mb-4">Info Corona atau COVID-19 di Negara Indonesia secara keseluran dari sabang sampai merauke</p>
+          <div class="row">
+            <div class="col-lg">
+              <div class="card">
+                <div class="card-body">
+                  <img src="img/senang.png" width="100" class="mb-4" alt="">
+                  <h4 class="card-title">Sembuh</h5>
+                  <h5 class="card-subtitle mb-2 text-muted">185 Orang</h6>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg">
+              <div class="card">
+                <div class="card-body">
+                  <img src="img/senang.png" width="100" class="mb-4" alt="">
+                  <h4 class="card-title">Sembuh</h5>
+                  <h5 class="card-subtitle mb-2 text-muted">185 Orang</h6>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg">
+              <div class="card">
+                <div class="card-body">
+                  <img src="img/senang.png" width="100" class="mb-4" alt="">
+                  <h4 class="card-title">Sembuh</h4>
+                  <h5 class="card-subtitle mb-2 text-muted">185 Orang</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </section>
+  <!-- end section dunia -->
+
+
+
+
+
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
